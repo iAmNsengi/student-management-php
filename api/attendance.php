472054d523024b242f91aa5bf57c0319386a2187
iAmNsengi <iamnsengi@icloud.com>
2022-11-23 08:41:32 +0200

    ft: add attendance api endpoint

---
+++ api/attendance.php
@@ -0,0 +1,18 @@
+<?php
+header('Content-Type: application/json');
+require_once "../config/database.php";
+require_once "../modules/attendance/Attendance.php";
+
+$database = new Database();
+$db = $database->getConnection();
+$attendance = new Attendance($db);
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $data = json_decode(file_get_contents('php://input'), true);
+    $result = $attendance->markAttendance(
+        $data['student_id'],
+        $data['date'],
+        $data['status']
+    );
+    echo json_encode(['success' => $result]);
+}
